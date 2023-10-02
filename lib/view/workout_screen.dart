e878d5c6107fbef0052bb5a38217d2989aa80385
Dola1122 <dolagamer2002@gmail.com>
2023-10-02 23:57:33 +0300

    workout screen : buttons, routine widget

--- lib/view/workout_screen.dart
+++ lib/view/workout_screen.dart
@@ -6,19 +6,167 @@ class WorkoutScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Workout"),centerTitle: true,),
-      body: Container(padding: EdgeInsets.all(16),
+      appBar: AppBar(
+        title: Text("Workout"),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-          Text("Quick Start"),
-            ElevatedButton(onPressed: (){},child: Text("Start Empty Workout"),),
-            Text("Routines"),
-            ElevatedButton(onPressed: (){},child: Text("New Routine"),),
-            Text("Saved Routines"),
-          ],),
-      )
-      ,
+            Text(
+              "Quick Start",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  fixedSize: Size(double.maxFinite, 40),
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.grey,
+                  side: BorderSide(
+                    color: Colors.black.withOpacity(0.2),
+                    width: 1,
+                  ),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(4)),
+                  elevation: 0),
+              onPressed: () {},
+              child: Text(
+                "Start Empty Workout",
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Text(
+              "Routines",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: Size(double.maxFinite, 40),
+                      backgroundColor: Colors.white,
+                      foregroundColor: Colors.grey,
+                      side: BorderSide(
+                        color: Colors.black.withOpacity(0.2),
+                        width: 1,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      elevation: 0,
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      "New Routine",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(double.maxFinite, 40),
+                      backgroundColor: Colors.white,
+                      foregroundColor: Colors.grey,
+                      side: BorderSide(
+                        color: Colors.black.withOpacity(0.2),
+                        width: 1,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      elevation: 0,
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      "Explore",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Text(
+              "Saved Routines",
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black.withOpacity(0.5)),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Container(
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4),
+                border: Border.fromBorderSide(
+                  BorderSide(
+                    color: Colors.black.withOpacity(0.2),
+                    width: 1,
+                  ),
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Chest & Back",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      IconButton(onPressed: () {}, icon: Icon(Icons.menu,color: Colors.blue,))
+                    ],
+                  ),
+                  Text(
+                    "Engaging your abs, slowly lift your upper body off the ground, curling forward until your torso is at a 45-degree angle.",
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black.withOpacity(0.3),),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  ElevatedButton(style: ElevatedButton.styleFrom(fixedSize: Size(double.maxFinite, 35)),onPressed: (){}, child: Text("Start Routine"))
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
